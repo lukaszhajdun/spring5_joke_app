a3de0f2325d1a4622cdfd09714f465466aa25707
lukasz <lukasz.hajdun@gmail.com>
2017-11-10 21:58:25 +0100

    Added JohnnyJokes class with jokes

---
+++ src/main/java/lukaszhajdun/repositories/JohnnyJokes.java
@@ -0,0 +1,231 @@
+package lukaszhajdun.repositories;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by Kroolik on 2017-11-10.
+ */
+public class JohnnyJokes {
+    private ArrayList<String> jokes = new ArrayList<>();
+
+    public JohnnyJokes(){
+
+        this.jokes.add("");
+
+        this.jokes.add(
+                "Na geografii:\n" +
+                "- Jasiu, gdzie leży Żywiec?\n" +
+                "- Na półce w sklepie, proszę pani.");
+
+        this.jokes.add(
+                "- Babciu, babciu, chodź tu i kopnij ten kalendarz.\n" +
+                "- Ale po co?\n" +
+                "- Bo tata powiedział, że jak babcia kopnie w kalendarz to mi kupi nowy rower.");
+
+        this.jokes.add(
+                "Tata uczy Jasia mówić.\n" +
+                "Tata:\n" +
+                "- Powiedz: \"tata\".\n" +
+                "Jasiu:\n" +
+                "- Mama.\n" +
+                "Tata:\n" +
+                "- Powiedz \"TATA\"!\n" +
+                "Jasiu:\n" +
+                "- Mama.\n" +
+                "Tata:\n" +
+                "- Powiedz KU*WA \"TATA\"!\n" +
+                "Jasiu:\n" +
+                "- KU*WA.\n" +
+                "W tym momencie mama wchodzi do domu i pyta się:\n" +
+                "Mama:\n" +
+                "- Kto cię tego nauczył?\n" +
+                "Jasiu:\n" +
+                "- Tata.");
+
+        this.jokes.add(
+                "Przychodzi Jasiu do piekarni i pyta:\n" +
+                "- Czy jest dwumetrowy chleb?\n" +
+                "- Nie, nie ma.\n" +
+                "Jasiowi zależało na tym chlebie i przychodził tak przez dwa miesiące." +
+                " Cały czas nie było chleba.\n" +
+                "Następnego dnia przychodzi i pyta:\n" +
+                "- Czy jest dwumetrowy chleb?\n" +
+                "- Tak jest.\n" +
+                "- To poproszę piętkę.");
+
+        this.jokes.add("Mama do Jasia:\n" +
+                "- Wiesz, ta dziewczyna, którą przyprowadziłeś, jest bardzo ładna i miła, " +
+                "ale wysyłając cię po osiemnastkę, miałam na myśli śmietanę.");
+
+        this.jokes.add(
+                "Ksiądz do Jasia przy spowiedzi:\n" +
+                "- Pamiętaj, grzeszyć można też myślami.\n" +
+                "- Tak, proszę księdza, ale co to za przyjemność..");
+
+        this.jokes.add(
+                "Mama pyta Jasia:\n" +
+                "- Brałeś prysznic?\n" +
+                "- Jak coś zginie to zawsze na mnie.");
+
+        this.jokes.add(
+                "- Moja siostra jest chora na szkarlatynę - oznajmia Jasiu w szkole.\n" +
+                "- To wracaj do domu, bo mi zarazisz całą klasę - mówi wychowawczyni.\n" +
+                "Po dwóch tygodniach Jasiu pojawia się na lekcjach.\n" +
+                "- I co, siostra wyzdrowiała? - pyta nauczycielka.\n" +
+                "- Nie wiem, jeszcze nie pisała.\n" +
+                "- To gdzie ona jest?\n" +
+                "- W Niemczech.");
+
+        this.jokes.add(
+                "- Jasiu!\n" +
+                "- Co tato?\n" +
+                "- Brałeś książkę z Kamasutrą?\n" +
+                "- No, brałem.\n" +
+                "- Dorysowałeś tam coś?\n" +
+                "- No... chyba tak.\n" +
+                "- To idź teraz matkę rozplącz.");
+
+        this.jokes.add(
+                "Mama pyta Jasia:\n" +
+                "- Jasiu, skąd masz te jajka?\n" +
+                "On na to:\n" +
+                "- Od sąsiada!\n" +
+                "- Czy on o tym wie?\n" +
+                "I on na to:\n" +
+                "- Tak, bo przed chwilą mnie wygonił ze swojego domu!");
+
+        this.jokes.add(
+                "Ksiądz do Jasia na lekcji religii:\n" +
+                "- Powiedz, Jasiu, kto wszystko widzi, wszystko słyszy i wszystko wie?\n" +
+                "- Nasza sąsiadka!");
+
+        this.jokes.add(
+                "Wbiega Jasiu do domu i krzyczy do taty, że alkohol drożeje. Po chwili pyta:\n" +
+                "- Tato, czy to oznacza, że będziesz mniej pił?\n" +
+                "Na to tata:\n" +
+                "- Nie Jasiu, to oznacza, że ty będziesz mniej jadł.");
+
+        this.jokes.add(
+                "Pani pyta Jasia:\n" +
+                "- Jasiu czym się niszczy domy?\n" +
+                "- Łopatką.\n" +
+                "- A drzewa?\n" +
+                "- Łopatką.\n" +
+                "- A babki z piasku? No, możesz już to powiedzieć.\n" +
+                "- Mogę?\n" +
+                "- Możesz.\n" +
+                "- Wk*rwia mnie pani");
+
+        this.jokes.add(
+                "Nauczycielka pyta Jasia:\n" +
+                "- Czemu znowu się spóźniłeś!?\n" +
+                "- Przepraszam, proszę pani, myłem zęby. Ale spokojnie, to nigdy się nie powtórzy!");
+
+        this.jokes.add(
+                "Przychodzi Jasiu do przedszkola i pyta:\n" +
+                "- Czy dzieci mogą mieć dzieci?\n" +
+                "Pani odpowiada:\n" +
+                "- W żadnym wypadku!\n" +
+                "Jasiu odwraca się do Małgosi i mówi:\n" +
+                "- I widzisz głupia!");
+
+        this.jokes.add(
+                "Jaś rozmawia z mamą:\n" +
+                "- Mamo, tata jeszcze nie wytrzeźwiał...\n" +
+                "- Czemu tak myślisz?\n" +
+                "- Bo goli lustro.");
+
+        this.jokes.add(
+                "Lekcja, pani bierze Jasia do odpowiedzi:\n" +
+                "- Jasiu, jaką literką na mapie oznaczamy wschód?\n" +
+                "- Eee...\n" +
+                "- Dobrze, siadaj, piątka.");
+
+        this.jokes.add(
+                "Do szkoły przyszła nowa nauczycielka. Szybko zasłynęła z tego, " +
+                "że zawsze ma idealnie wypucowane lakierki - jak lustro.\n" +
+                "Pewnego dnia Jasiu podchodzi do tej nauczycielki, patrzy się na jej lakierki i ze spokojem mówi:\n" +
+                "- Ma pani śliczne, różowe majteczki.\n" +
+                "- Jasiu, jak możesz się tam patrzeć! - krzyczy oburzona nauczycielka.\n" +
+                "Następnego dnia sytuacja się powtarza.\n" +
+                "- Ma pani śliczne, żółte majteczki.\n" +
+                "- Jasiu, jutro masz przyjść z rodzicami! - rozkazuje nauczycielka.\n" +
+                "Kolejnego dnia Jasiu przychodzi z rodzicami. Pani zaczyna mówić:\n" +
+                "- Proszę państwa, pański syn cały czas patrzy mi się na...\n" +
+                "- Droga pani... - zaczyna ojciec Jasia - Takie piękne lakierki, a taka wielka dziura w nich...");
+
+        this.jokes.add(
+                "Jasiu przychodzi do mamy ze spuchniętą twarzą.\n" +
+                "- Synku, co się stało? - pyta mama.\n" +
+                "- Mucha.\n" +
+                "- Co, ugryzła?\n" +
+                "- Nie. Usiadła na mojej twarzy i tata ją łopatą zabił.");
+
+        this.jokes.add(
+                "Jaś idzie przez przez las i spotyka wróżkę. Ta mówi:\n" +
+                "- Spełnię twoje dwa życzenia.\n" +
+                "Jasiu na to:\n" +
+                "- Widzisz to drzewo?\n" +
+                "- Tak.\n" +
+                "- To wbij w nie gwoździa, tak aby nikt go nie wyjął.\n" +
+                "Potem wróżka pyta:\n" +
+                "- A drugie życzenie?\n" +
+                "- Teraz go wyjmij.");
+
+        this.jokes.add(
+                "Przychodzi Jasiu do domu ze szkoły i mówi:\n" +
+                "- Mamo! Zostałem dziś wyróżniony.\n" +
+                "- Za co, Jasiu? - pyta mama.\n" +
+                "- Pani powiedziała że cała nasza klasa to debile, a ja największy.");
+
+        this.jokes.add(
+                "W pewnej miejscowości ktoś kradł kowalowi jabłka. Ten ukrył się w sadzie i czekał na złodzieja. Gdy on wdrapywał się na drzewo, kowal jako chłop ze stali ścisnął złodziejaszka za jaja i rzekł:\n" +
+                "- Kto ty?\n" +
+                "Cisza... Ścisnął jeszcze mocniej i pyta:\n" +
+                "- Kto ty!?\n" +
+                "Cisza, więc ścisnął najmocniej jak tylko mógł i powiedział:\n" +
+                "- Kto ty!?\n" +
+                "- Jaśko!\n" +
+                "- Jaki Jaśko? Bo jest Jaśko młynarz, Jaśko stolarz, Jaśko drwal...\n" +
+                "- Jaśko niemowa...");
+
+        this.jokes.add(
+                "Jaś pyta się taty:\n" +
+                "- Tato, co to są szanty?\n" +
+                "- Pirackie piosenki, Jasiu.\n" +
+                "- Aaa, to ja mam na komputerze same szanty.");
+
+        this.jokes.add(
+                "Przychodzi Jasiu do sklepu, podaje sprzedawczyni kankę i mówi:\n" +
+                "- Poproszę trzy litry miodu.\n" +
+                "Sprzedawczyni bierze kankę, nalewa miodu, podaje Jasiowi, a ten mówi:\n" +
+                "- Tata jutro przyjdzie i zapłaci.\n" +
+                "Na co sprzedawczyni:\n" +
+                "- Nie, tak nie można!\n" +
+                "Zabiera chłopcu kankę, wylewa miód i oddaje pustą. Jasiu wychodzi na ulicę, zagląda do kanki i mówi:\n" +
+                "- No i miał rację tata, na trzy kanapki wystarczy.");
+
+        this.jokes.add(
+                "Supermarket. 6-letni Jaś patrzy łakomym wzrokiem w lodówkę z lodami:\n" +
+                "- Mamo, mogę loda?\n" +
+                "- Nie.\n" +
+                "- Mamoooooooo!\n" +
+                "- Powiedziałam coś! Nie! - matka pozostaje nieugięta.\n" +
+                "Oczy Jasia kierują się na ojca. Ojciec na to zbolałym głosem:\n" +
+                "- Jak mama mówi, że loda nie będzie, to uwierz mi, wie co mówi.");
+
+        this.jokes.add(
+                "Jasiu mówi do ojca:\n" +
+                "- Tato, masz dzisiaj takie małe zebranie rodziców w szkole.\n" +
+                "- Co masz na myśli, mówiąc \"małe\"?\n" +
+                "- No, ja, ty, wychowawczyni i dyrektor...");
+
+
+    }
+
+
+    public String getRandomJoke() {
+        return this.jokes.get(ThreadLocalRandom.current().nextInt(1, this.jokes.size()));
+    }
+}
